ff49298dd24c5281e28e4e87ce314050159c3e45
ip133225 <ip133225@students.davenport.k12.ia.us>
2017-12-04 11:28:44 -0600

    sum

---
+++ Sum Of Numbers/Sum Of Numbers/Form1.Designer.vb
@@ -0,0 +1,72 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnEnterSales = New System.Windows.Forms.Button()
+        Me.lblInteger = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnEnterSales
+        '
+        Me.btnEnterSales.Location = New System.Drawing.Point(35, 70)
+        Me.btnEnterSales.Name = "btnEnterSales"
+        Me.btnEnterSales.Size = New System.Drawing.Size(92, 40)
+        Me.btnEnterSales.TabIndex = 0
+        Me.btnEnterSales.Text = "Enter Numbers"
+        Me.btnEnterSales.UseVisualStyleBackColor = True
+        '
+        'lblInteger
+        '
+        Me.lblInteger.Location = New System.Drawing.Point(68, 44)
+        Me.lblInteger.Name = "lblInteger"
+        Me.lblInteger.Size = New System.Drawing.Size(100, 23)
+        Me.lblInteger.TabIndex = 1
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(143, 70)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(92, 40)
+        Me.btnExit.TabIndex = 2
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 130)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.lblInteger)
+        Me.Controls.Add(Me.btnEnterSales)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnEnterSales As System.Windows.Forms.Button
+    Friend WithEvents lblInteger As System.Windows.Forms.Label
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+
+End Class
---
+++ Sum Of Numbers/Sum Of Numbers/Form1.vb
@@ -0,0 +1,39 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub lblTotal_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub btnEnterSales_Click(sender As Object, e As EventArgs) Handles btnEnterSales.Click
+        Const intNUM_DAYS As Integer = 5
+        Dim intCount As Integer = 1
+        Dim decSales As Decimal = 0
+        Dim decTotal As Decimal = 0
+        Dim strInput As String
+
+        Do While intCount <= intNUM_DAYS
+
+            strInput = InputBox("Enter a positive integer value. ")
+
+            If Decimal.TryParse(strInput, decSales) Then
+
+                decTotal += decSales
+
+                intCount += 1
+            Else
+
+                MessageBox.Show("The sum of the numbers 1 though 10 is 55")
+            End If
+        Loop
+
+        lblInteger.Text = decTotal.ToString("c")
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Application.Exit()
+    End Sub
+End Class
