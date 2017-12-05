14aa9f632fccce0723c387dc19a8672f97527d6e
ip133225 <ip133225@students.davenport.k12.ia.us>
2017-12-05 11:26:30 -0600

    Rock ....

---
+++ Rock Paper Scissors/Rock Paper Scissors/Form1.Designer.vb
@@ -0,0 +1,118 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.btnGo = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.RadioButton3)
+        Me.GroupBox1.Controls.Add(Me.RadioButton2)
+        Me.GroupBox1.Controls.Add(Me.RadioButton1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 41)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(195, 100)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Choose Your Throw"
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(6, 54)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(51, 17)
+        Me.RadioButton1.TabIndex = 0
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Rock"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(63, 54)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(53, 17)
+        Me.RadioButton2.TabIndex = 1
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Paper"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(122, 54)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(64, 17)
+        Me.RadioButton3.TabIndex = 2
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "Scissors"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'btnGo
+        '
+        Me.btnGo.Location = New System.Drawing.Point(12, 147)
+        Me.btnGo.Name = "btnGo"
+        Me.btnGo.Size = New System.Drawing.Size(75, 23)
+        Me.btnGo.TabIndex = 1
+        Me.btnGo.Text = "Go!"
+        Me.btnGo.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label1.Location = New System.Drawing.Point(12, 179)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(195, 23)
+        Me.Label1.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(220, 211)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnGo)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Rock Paper Scissors"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents RadioButton3 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton2 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton1 As System.Windows.Forms.RadioButton
+    Friend WithEvents btnGo As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+
+End Class
---
+++ Rock Paper Scissors/Rock Paper Scissors/Form1.vb
@@ -0,0 +1,37 @@
+﻿Public Class Form1
+
+    Private Sub btnGo_Click(sender As Object, e As EventArgs) Handles btnGo.Click
+        Randomize()
+
+        Dim number As Integer
+
+        Const ROCK As Integer = 1
+        Const PAPER As Integer = 2
+        Const SCISSORS As Integer = 3
+
+        number = (3 * Rnd())
+
+        If RadioButton1.Checked And number = ROCK Then
+            Label1.Text = "The Computer Threw. Draw!"
+        ElseIf RadioButton1.Checked And number = PAPER Then
+            Label1.Text = "The Computer Threw. You Lose!"
+        ElseIf RadioButton1.Checked And number = SCISSORS Then
+            Label1.Text = "The Computer Threw. You Win!"
+
+        ElseIf RadioButton2.Checked And number = ROCK Then
+            Label1.Text = "The Computer Threw. You Win!"
+        ElseIf RadioButton2.Checked And number = PAPER Then
+            Label1.Text = "The Computer Threw. Draw!"
+        ElseIf RadioButton2.Checked And number = SCISSORS Then
+            Label1.Text = "The Computer Threw. You Win!"
+
+        ElseIf RadioButton3.Checked And number = ROCK Then
+            Label1.Text = "The Computer Threw. You Lose!"
+        ElseIf RadioButton3.Checked And number = PAPER Then
+            Label1.Text = "The Computer Threw. You Win!"
+        ElseIf RadioButton3.Checked And number = SCISSORS Then
+            Label1.Text = "The Computer Threw. Draw!"
+
+        End If
+    End Sub
+End Class
